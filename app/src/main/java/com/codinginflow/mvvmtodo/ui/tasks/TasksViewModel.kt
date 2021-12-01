0dd6fb8ae1af2a50b1ac7411e0ab0adc32c4a9b3
Alvin Dizon <admdizon@scentregroup.com>
2021-12-01 14:09:01 +0800

    Part 5: Add adapter code
    Signed-off-by: Alvin Dizon <admdizon@scentregroup.com>

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -1,10 +1,13 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
